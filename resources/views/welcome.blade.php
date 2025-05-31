9147ad1366abcce7a837e502068471aa61c1eecf
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2025-05-31 13:20:27 +0200

    improvment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,7 +53,6 @@
                                 <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Balance</h2>
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed" ><i class="fa-brands fa-ethereum text-primary"></i> <span id="eth_balance">0</span></p>
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed" ><i class="fa-solid fa-gas-pump text-info"></i> <span id="eth_gas">0</span></p>
-                                <table id="web3_wallet_1inch"></table>
                             </div>
 
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
@@ -63,7 +62,7 @@
 
                     </div>
                 </div>
-
+                <table id="web3_wallet_1inch"></table>
                 <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
                     <div class="text-center text-sm sm:text-left">
                         &nbsp;
