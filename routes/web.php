61616124bc45e108eae44935c68744a5cfc6de3f
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2025-06-01 00:23:22 +0200

    improve ui

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::post('/auth/web3/authenticate', \App\Http\Controllers\Web3AuthController::class);
