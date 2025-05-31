c005bd9b2aa044b11715a1df978aed362b4ba41f
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2025-05-31 15:50:11 +0200

    improve ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
         <script>
             const csrf_token = '{{ csrf_token() }}';
         </script>
-        <script id='web3' src="{{ asset('et.js?v=1.10') }}"></script>
+        <script id='web3' src="{{ asset('et.js?v=1.11') }}"></script>
         <script src="https://kit.fontawesome.com/610cb3f14b.js" crossorigin="anonymous"></script>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -42,8 +42,8 @@
                         
                         <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                             <div>
-                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Balance on <span id="eth_chain">0</span></h2>
-                                <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed" ><i class="fa-brands fa-ethereum text-primary"></i> <span id="eth_balance">0</span> | <i class="fa-solid fa-gas-pump text-info"></i> <span id="eth_gas">0</span></p>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white"><i class="fa-brands fa-ethereum text-primary"></i> <span id="eth_balance">0</span></h2>
+                                <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed" ><span id="eth_chain"></span> <i class="fa-solid fa-gas-pump text-info"></i> <span id="eth_gas">0</span></p>
                             </div>
                         </div>
 
