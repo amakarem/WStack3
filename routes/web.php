2dc080a18b231a61cb9e79cf9ad4ad9371de1d64
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2025-05-31 09:52:44 +0200

    install auth contoller

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::get('/home', function () {
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
