4b688ff1be2d347655df9f6004a06c4eee973bcf
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2025-05-31 12:19:55 +0200

    integrate 1inch API

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::post('/auth/web3/authenticate', \App\Http\Controllers\Web3AuthController::class);
-Route::post('/web3/wallet', \App\Http\Controllers\MainController::class, 'wallet');
+Route::post('/web3/wallet{address}', 'MainController@wallet')->name('wallet');
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
