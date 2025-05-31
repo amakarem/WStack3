cb0b78e66db2f3c331f2be68a86baaae8eebd075
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2025-05-31 22:54:29 +0200

    swap now

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,10 +24,10 @@
             
         </style>
     </head>
-    <body class="antialiased" id="app">
-        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+    <body id="app">
+        <div class="container">
 
-            <div class="max-w-7xl mx-auto p-6 lg:p-8">
+            <div class="row justify-content-center">
                 <div class="flex justify-center">
                     <img src="images/icons/eth.svg" style="max-width: 6rem;">
                 </div>
