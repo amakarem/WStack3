7876ea15d169911c8292e05bf7d236ee43a93a80
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2025-05-31 10:23:17 +0200

    improve

--- app/Http/Controllers/Web3AuthController.php
+++ app/Http/Controllers/Web3AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
+
 use App\User;
 use Elliptic\EC;
 use Illuminate\Support\Facades\Auth;
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::post('/auth/web3/authenticate', \App\Http\Controllers\Web3AuthController::class);
 
+Route::post('/auth/web3/authenticate', \App\Http\Controllers\Web3AuthController::class);
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
