19c5bc79b456ae9b5dce1d3c8e5f7078c8dc5e7f
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2025-05-31 02:22:16 +0200

    fix login option

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
                     @auth
                         <a href="{{ route('logout') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log Out</a>
                     @else
-                        <button class="btn btn-warning" onclick="loginWeb3()">{{ __('Connect MetaMask') }}</button>
+                        <button class="btn btn-warning" onclick="loginWeb3()" style="background: #ffffff;padding: 10px;">{{ __('Connect MetaMask') }}</button>
                     @endauth
                     </div>
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
