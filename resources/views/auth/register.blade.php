93b213e94da3bae75609a58b90ecf405fdda9a2b
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2025-06-01 00:25:16 +0200

    register with metamask

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,6 +12,11 @@
                         @csrf
 
                         <div class="row mb-3">
+                            <div class="col text-center">
+                                <button class="btn btn-warning" onclick="loginWeb3()">{{ __('Continue with MetaMask') }}</button>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
                             <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
