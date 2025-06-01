b3eaae7f1555a8bf39370c3a04ff01117ef132c1
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2025-06-01 03:40:42 +0200

    fix login

--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
 });
+
+Auth::routes();
 
 Route::post('/auth/web3/authenticate', \App\Http\Controllers\Web3AuthController::class);
 Route::post('/web3/wallet/{address}', [App\Http\Controllers\API1inch::class, 'wallet']);
 Route::post('/web3/getswapquote', [App\Http\Controllers\API1inch::class, 'getswapquote']);
 Route::post('/web3/swapnow', [App\Http\Controllers\API1inch::class, 'swapnow']);
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+
+Route::get('/', function () {
+    return view('home');
 });
 
 Route::get('/modal/swap', function () {
     return view('model.swap');
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
