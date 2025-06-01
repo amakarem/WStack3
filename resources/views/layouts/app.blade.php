f1e7663b1a871eb1d7f1656240338958a2b61c57
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2025-06-01 04:22:27 +0200

    update app name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Laravel web3 dex</title>
+        <title>WStack3</title>
         <script>
             const csrf_token = '{{ csrf_token() }}';
         </script>
